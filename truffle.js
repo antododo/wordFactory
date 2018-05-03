ed3dea6cff9f811d26946a3b33ee05241557f4cb
pl4ym0re <pl4ym0re@gmail.com>
2018-05-03 09:02:30 -0400

    setup truffle.js with ganache

--- truffle.js
+++ truffle.js
@@ -1,10 +1,15 @@
 module.exports = {
-  migrations_directory: "./migrations",
+  migrations_directory: './migrations',
   networks: {
+    ganache: {
+      host: 'localhost',
+      port: 7545,
+      network_id: '*'
+    },
     development: {
-      host: "localhost",
+      host: 'localhost',
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: '*' // Match any network id
     }
   }
 };
