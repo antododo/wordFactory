ea6f0973f2f54d987c3092587967cd18ccd918b4
pl4ym0re <pl4ym0re@gmail.com>
2018-05-29 09:52:53 -0400

    truffle config for rinkeby

--- truffle.js
+++ truffle.js
@@ -10,6 +10,12 @@ module.exports = {
       host: 'localhost',
       port: 8545,
       network_id: '*' // Match any network id
+    },
+    rinkeby: {
+      host: 'localhost',
+      port: 8545,
+      network_id: 4, //rinkeby test network
+      gas: 4700000
     }
   }
 };
